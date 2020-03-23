129f7c735921327f4741dd9fd849b715803d9981
user167484 <user167484@MU045.local>
2020-03-23 20:06:03 +0530

    First Commit

---
+++ TestSwiftUIComponents/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwiftUIComponents
+//
+//  Created by user167484 on 3/23/20.
+//  Copyright © 2020 Allen Savio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
